69219d74c73f7d509ee3b82ab6827a5eb3b232b0
Rafael Angonese <rafael.angonese09@gmail.com>
2025-08-05 00:46:40 -0300

    :coffin: Refactor remove dead code examples

---
+++ src/examples/heart.ts
@@ -0,0 +1,60 @@
+import { PixelArt } from "../types";
+
+export const heartPixelArt: PixelArt = {
+    pixels: [
+        // Coração pequeno
+        { x: 10, y: 10, color: 26 }, // Dark Pink
+        { x: 11, y: 10, color: 26 },
+        { x: 12, y: 10, color: 26 },
+        { x: 13, y: 10, color: 26 },
+        { x: 14, y: 10, color: 26 },
+
+        { x: 9, y: 11, color: 26 },
+        { x: 10, y: 11, color: 27 }, // Pink
+        { x: 11, y: 11, color: 27 },
+        { x: 12, y: 11, color: 27 },
+        { x: 13, y: 11, color: 27 },
+        { x: 14, y: 11, color: 27 },
+        { x: 15, y: 11, color: 26 },
+
+        { x: 8, y: 12, color: 26 },
+        { x: 9, y: 12, color: 27 },
+        { x: 10, y: 12, color: 28 }, // Light Pink
+        { x: 11, y: 12, color: 28 },
+        { x: 12, y: 12, color: 28 },
+        { x: 13, y: 12, color: 28 },
+        { x: 14, y: 12, color: 28 },
+        { x: 15, y: 12, color: 27 },
+        { x: 16, y: 12, color: 26 },
+
+        { x: 8, y: 13, color: 26 },
+        { x: 9, y: 13, color: 27 },
+        { x: 10, y: 13, color: 28 },
+        { x: 11, y: 13, color: 28 },
+        { x: 12, y: 13, color: 28 },
+        { x: 13, y: 13, color: 28 },
+        { x: 14, y: 13, color: 28 },
+        { x: 15, y: 13, color: 27 },
+        { x: 16, y: 13, color: 26 },
+
+        { x: 9, y: 14, color: 26 },
+        { x: 10, y: 14, color: 27 },
+        { x: 11, y: 14, color: 28 },
+        { x: 12, y: 14, color: 28 },
+        { x: 13, y: 14, color: 28 },
+        { x: 14, y: 14, color: 27 },
+        { x: 15, y: 14, color: 26 },
+
+        { x: 10, y: 15, color: 26 },
+        { x: 11, y: 15, color: 27 },
+        { x: 12, y: 15, color: 27 },
+        { x: 13, y: 15, color: 27 },
+        { x: 14, y: 15, color: 26 },
+
+        { x: 11, y: 16, color: 26 },
+        { x: 12, y: 16, color: 26 },
+        { x: 13, y: 16, color: 26 },
+
+        { x: 12, y: 17, color: 26 },
+    ]
+};
